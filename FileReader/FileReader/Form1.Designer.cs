6bec9a58421a638818cec655448b17b37f477819
Addisu <72408025+addispupi@users.noreply.github.com>
2021-03-17 22:01:24 +0300

    Add files via upload

---
+++ FileReader/FileReader/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace FileReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSetPath = new System.Windows.Forms.Button();
+            this.txtSourceFile = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnDestination = new System.Windows.Forms.Button();
+            this.txtDestinationFile = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnMove = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.txtOpenedFile = new System.Windows.Forms.RichTextBox();
+            this.btnBold = new System.Windows.Forms.Button();
+            this.btnItalic = new System.Windows.Forms.Button();
+            this.btnACenter = new System.Windows.Forms.Button();
+            this.btnALeft = new System.Windows.Forms.Button();
+            this.btnARight = new System.Windows.Forms.Button();
+            this.btnUnderline = new System.Windows.Forms.Button();
+            this.btnStrike = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(466, 19);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(101, 33);
+            this.btnCopy.TabIndex = 0;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label1.Location = new System.Drawing.Point(21, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Text File Reader and Updater";
+            // 
+            // btnSetPath
+            // 
+            this.btnSetPath.Location = new System.Drawing.Point(466, 11);
+            this.btnSetPath.Name = "btnSetPath";
+            this.btnSetPath.Size = new System.Drawing.Size(106, 28);
+            this.btnSetPath.TabIndex = 5;
+            this.btnSetPath.Text = "Source";
+            this.btnSetPath.UseVisualStyleBackColor = true;
+            this.btnSetPath.Click += new System.EventHandler(this.btnSetPath_Click);
+            // 
+            // txtSourceFile
+            // 
+            this.txtSourceFile.Location = new System.Drawing.Point(13, 16);
+            this.txtSourceFile.Name = "txtSourceFile";
+            this.txtSourceFile.Size = new System.Drawing.Size(438, 20);
+            this.txtSourceFile.TabIndex = 6;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnDestination
+            // 
+            this.btnDestination.Location = new System.Drawing.Point(466, 45);
+            this.btnDestination.Name = "btnDestination";
+            this.btnDestination.Size = new System.Drawing.Size(106, 28);
+            this.btnDestination.TabIndex = 7;
+            this.btnDestination.Text = "Destination";
+            this.btnDestination.UseVisualStyleBackColor = true;
+            this.btnDestination.Click += new System.EventHandler(this.btnDestination_Click);
+            // 
+            // txtDestinationFile
+            // 
+            this.txtDestinationFile.Location = new System.Drawing.Point(13, 50);
+            this.txtDestinationFile.Name = "txtDestinationFile";
+            this.txtDestinationFile.Size = new System.Drawing.Size(438, 20);
+            this.txtDestinationFile.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSetPath);
+            this.groupBox1.Controls.Add(this.btnDestination);
+            this.groupBox1.Controls.Add(this.txtDestinationFile);
+            this.groupBox1.Controls.Add(this.txtSourceFile);
+            this.groupBox1.Location = new System.Drawing.Point(12, 63);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(587, 79);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Set file director and destination file";
+            // 
+            // btnMove
+            // 
+            this.btnMove.Location = new System.Drawing.Point(359, 58);
+            this.btnMove.Name = "btnMove";
+            this.btnMove.Size = new System.Drawing.Size(101, 33);
+            this.btnMove.TabIndex = 10;
+            this.btnMove.Text = "Move";
+            this.btnMove.UseVisualStyleBackColor = true;
+            this.btnMove.Click += new System.EventHandler(this.btnMove_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(359, 19);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(101, 33);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnStrike);
+            this.groupBox2.Controls.Add(this.btnUnderline);
+            this.groupBox2.Controls.Add(this.btnARight);
+            this.groupBox2.Controls.Add(this.btnALeft);
+            this.groupBox2.Controls.Add(this.btnACenter);
+            this.groupBox2.Controls.Add(this.btnItalic);
+            this.groupBox2.Controls.Add(this.btnBold);
+            this.groupBox2.Controls.Add(this.btnDisplay);
+            this.groupBox2.Controls.Add(this.btnUpdate);
+            this.groupBox2.Controls.Add(this.btnCopy);
+            this.groupBox2.Controls.Add(this.btnMove);
+            this.groupBox2.Controls.Add(this.btnDelete);
+            this.groupBox2.Location = new System.Drawing.Point(12, 365);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(587, 114);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(8, 19);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(101, 33);
+            this.btnDisplay.TabIndex = 13;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(8, 58);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(101, 33);
+            this.btnUpdate.TabIndex = 12;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // txtOpenedFile
+            // 
+            this.txtOpenedFile.Location = new System.Drawing.Point(12, 148);
+            this.txtOpenedFile.Name = "txtOpenedFile";
+            this.txtOpenedFile.Size = new System.Drawing.Size(587, 211);
+            this.txtOpenedFile.TabIndex = 13;
+            this.txtOpenedFile.Text = "";
+            // 
+            // btnBold
+            // 
+            this.btnBold.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBold.Location = new System.Drawing.Point(126, 19);
+            this.btnBold.Name = "btnBold";
+            this.btnBold.Size = new System.Drawing.Size(36, 33);
+            this.btnBold.TabIndex = 14;
+            this.btnBold.Text = "B";
+            this.btnBold.UseVisualStyleBackColor = true;
+            this.btnBold.Click += new System.EventHandler(this.btnBold_Click);
+            // 
+            // btnItalic
+            // 
+            this.btnItalic.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnItalic.Location = new System.Drawing.Point(126, 58);
+            this.btnItalic.Name = "btnItalic";
+            this.btnItalic.Size = new System.Drawing.Size(36, 33);
+            this.btnItalic.TabIndex = 15;
+            this.btnItalic.Text = "i";
+            this.btnItalic.UseVisualStyleBackColor = true;
+            this.btnItalic.Click += new System.EventHandler(this.btnItalic_Click);
+            // 
+            // btnACenter
+            // 
+            this.btnACenter.Font = new System.Drawing.Font("Wingdings 2", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.btnACenter.Location = new System.Drawing.Point(253, 39);
+            this.btnACenter.Name = "btnACenter";
+            this.btnACenter.Size = new System.Drawing.Size(31, 33);
+            this.btnACenter.TabIndex = 16;
+            this.btnACenter.Text = "";
+            this.btnACenter.UseVisualStyleBackColor = true;
+            this.btnACenter.Click += new System.EventHandler(this.btnACenter_Click);
+            // 
+            // btnALeft
+            // 
+            this.btnALeft.Font = new System.Drawing.Font("Wingdings 3", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.btnALeft.Location = new System.Drawing.Point(218, 39);
+            this.btnALeft.Name = "btnALeft";
+            this.btnALeft.Size = new System.Drawing.Size(29, 33);
+            this.btnALeft.TabIndex = 17;
+            this.btnALeft.Text = "";
+            this.btnALeft.UseVisualStyleBackColor = true;
+            this.btnALeft.Click += new System.EventHandler(this.btnALeft_Click);
+            // 
+            // btnARight
+            // 
+            this.btnARight.Font = new System.Drawing.Font("Wingdings 3", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.btnARight.Location = new System.Drawing.Point(290, 39);
+            this.btnARight.Name = "btnARight";
+            this.btnARight.Size = new System.Drawing.Size(27, 33);
+            this.btnARight.TabIndex = 18;
+            this.btnARight.Text = "";
+            this.btnARight.UseVisualStyleBackColor = true;
+            this.btnARight.Click += new System.EventHandler(this.btnARight_Click);
+            // 
+            // btnUnderline
+            // 
+            this.btnUnderline.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUnderline.Location = new System.Drawing.Point(171, 19);
+            this.btnUnderline.Name = "btnUnderline";
+            this.btnUnderline.Size = new System.Drawing.Size(41, 33);
+            this.btnUnderline.TabIndex = 19;
+            this.btnUnderline.Text = "U";
+            this.btnUnderline.UseVisualStyleBackColor = true;
+            this.btnUnderline.Click += new System.EventHandler(this.btnUnderline_Click);
+            // 
+            // btnStrike
+            // 
+            this.btnStrike.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Strikeout, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStrike.Location = new System.Drawing.Point(171, 58);
+            this.btnStrike.Name = "btnStrike";
+            this.btnStrike.Size = new System.Drawing.Size(41, 33);
+            this.btnStrike.TabIndex = 20;
+            this.btnStrike.Text = "S";
+            this.btnStrike.UseVisualStyleBackColor = true;
+            this.btnStrike.Click += new System.EventHandler(this.btnStrike_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 491);
+            this.Controls.Add(this.txtOpenedFile);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "File Manipulator App";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSetPath;
+        private System.Windows.Forms.TextBox txtSourceFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnDestination;
+        private System.Windows.Forms.TextBox txtDestinationFile;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnMove;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.RichTextBox txtOpenedFile;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Button btnItalic;
+        private System.Windows.Forms.Button btnBold;
+        private System.Windows.Forms.Button btnACenter;
+        private System.Windows.Forms.Button btnARight;
+        private System.Windows.Forms.Button btnALeft;
+        private System.Windows.Forms.Button btnStrike;
+        private System.Windows.Forms.Button btnUnderline;
+    }
+}
+
---
+++ FileReader/FileReader/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FileReader
+{
+    public partial class Form1 : Form
+    {
+        Font boldFont = new Font("Arial", 10, FontStyle.Bold);
+        Font italicFont = new Font("Arial", 10, FontStyle.Italic);
+        Font strikeFont = new Font("Arial", 10, FontStyle.Strikeout);
+        Font underlineFont = new Font("Arial", 10, FontStyle.Underline);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSetPath_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.Title = "Select a File";
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "Text Fies (*.txt) | *.txt";
+            openFileDialog1.FilterIndex = 1;
+
+            if (openFileDialog1.ShowDialog() != DialogResult.Cancel)
+                txtSourceFile.Text = openFileDialog1.FileName;
+            else
+                txtSourceFile.Text = "";
+        }
+
+
+        private void btnDestination_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Title = "Select a Destination Directory";
+            saveFileDialog1.Filter = "Text Fies (*.txt) | *.txt";
+            saveFileDialog1.FilterIndex = 1;
+            saveFileDialog1.OverwritePrompt = true;
+                       
+            if (saveFileDialog1.ShowDialog() != DialogResult.Cancel)
+                txtDestinationFile.Text = saveFileDialog1.FileName;
+            else
+                txtDestinationFile.Text = "";
+        }
+
+        bool SourceFileExists()
+        {
+            if (!System.IO.File.Exists(txtSourceFile.Text))
+            {
+                MessageBox.Show("The Source File Doesen't Exist");
+                return false;
+            }
+            else
+                return true;
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            if (!SourceFileExists())
+                return;
+            System.IO.File.Copy(txtSourceFile.Text, txtDestinationFile.Text);
+            MessageBox.Show("The file has been successfully copied!");
+        }
+
+        private void btnMove_Click(object sender, EventArgs e)
+        {
+            if (!SourceFileExists())
+                return;
+            System.IO.File.Move(txtSourceFile.Text, txtDestinationFile.Text);
+            MessageBox.Show("The file has been successfully moved!");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (!SourceFileExists())
+                return;
+            if(MessageBox.Show("Are you sure you want to delete?", "Delete Verfication", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                System.IO.File.Delete(txtSourceFile.Text);
+                MessageBox.Show("The file has been successfully deleted!");
+            }
+            
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            string pathfile = @txtSourceFile.Text;
+            txtOpenedFile.Text = File.ReadAllText(pathfile);
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string content = txtOpenedFile.Text;
+            string pathfile = @txtSourceFile.Text;
+
+            File.WriteAllText(pathfile, content);
+            MessageBox.Show("The file has been successfully updated!");
+        }
+
+        private void btnBold_Click(object sender, EventArgs e)
+        {
+            txtOpenedFile.SelectionFont = boldFont;
+        }
+
+        private void btnItalic_Click(object sender, EventArgs e)
+        {
+            txtOpenedFile.SelectionFont = italicFont;
+        }
+
+        private void btnALeft_Click(object sender, EventArgs e)
+        {
+            txtOpenedFile.SelectionAlignment = HorizontalAlignment.Left;
+        }
+
+        private void btnACenter_Click(object sender, EventArgs e)
+        {
+            txtOpenedFile.SelectionAlignment = HorizontalAlignment.Center;
+        }
+
+        private void btnARight_Click(object sender, EventArgs e)
+        {
+            txtOpenedFile.SelectionAlignment = HorizontalAlignment.Right;
+        }
+
+        private void btnUnderline_Click(object sender, EventArgs e)
+        {
+            txtOpenedFile.SelectionFont = underlineFont;
+        }
+
+        private void btnStrike_Click(object sender, EventArgs e)
+        {
+            txtOpenedFile.SelectionFont = strikeFont;
+        }
+    }
+}
